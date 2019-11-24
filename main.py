e90fe27fc1df4b615937a8a153cbed75030f371f
pomidor <mishpzr@yandex.ru>
2019-11-24 16:09:00 +0300

    take youtube data

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import os
+import os, json, re, urllib.request
+from pytube import YouTube
 
-api_key= os.environ.get('api_key')
+api_key = os.environ.get('API_KEY')
+
+video_id = "g_PVrBGStBI"  #ZkYOvViSx3E
+
+url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={video_id}&key={api_key}"
+
+json_url = urllib.request.urlopen(url)
+data = json.loads(json_url.read())
+
+print(data)
