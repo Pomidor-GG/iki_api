8e4d389111175c45f500cd7d8f4427eee4ccd693
Pomidor-GG <mishpzr@yandex.ru>
2020-03-30 19:57:24 +0300

    working version

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os, json, re, urllib.request
 from pytube import YouTube
 
 api_key = os.environ.get('API_KEY')
+channel_id = "UCJymPbg-0_yc-63wqc0WtIQ"
+twelve_videos = []
+list_url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={channel_id}&maxResults=12&order=date&type=video&key={api_key}"
 
-video_id = "g_PVrBGStBI"  #ZkYOvViSx3E
-
-url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={video_id}&key={api_key}"
-
-json_url = urllib.request.urlopen(url)
+json_url = urllib.request.urlopen(list_url)
 data = json.loads(json_url.read())
-
-print(data)
+for i in data['items']:
+    twelve_videos.append(i['snippet']['thumbnails']['medium']['url'])
+print(twelve_videos)
